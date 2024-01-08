b5dd0d59cc5f76f4fbec27ecb9536d5ef7f3a20b
jpaul-meta <155984997+jpaul-meta@users.noreply.github.com>
2024-01-08 22:14:15 +0800

    updated

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "Joshua"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "JETH"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "JE"
 )
 
 var ID ids.ID
